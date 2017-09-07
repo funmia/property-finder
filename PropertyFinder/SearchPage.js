1faca146dbda2dcc9ee40b5fbcce5ececab53e50
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-09-07 22:37:11 +0100

    updates styling for search results

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -10,6 +10,7 @@ import {
   ActivityIndicator,
   Image,
   } from 'react-native';
+  import SearchResults from './SearchResults';
 
   function urlForQueryAndPage(key, value, pageNumber) {
   const data = {
@@ -59,11 +60,15 @@ import {
       const query = urlForQueryAndPage('place_name', this.state.searchString, 1);
       this._executeQuery(query);
     };
-    
+
     _handleResponse = (response) => {
       this.setState({ isLoading: false , message: '' });
       if (response.application_response_code.substr(0, 1) === '1') {
-        console.log('Properties found: ' + response.listings.length);
+        this.props.navigator.push({
+          title: 'Results',
+          component: SearchResults,
+          passProps: {listings: response.listings}
+        });
       } else {
         this.setState({ message: 'Location not recognized; please try again.'});
       }
